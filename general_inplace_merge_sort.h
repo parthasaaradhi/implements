18ffb31ae66766f4177ddc38cc0e20605f1b08a4
venky <venky@g7>
2021-12-25 07:06:24 +0530

    Optimised parameter and stack memory usage by internalisation.

--- general_inplace_merge_sort.h
+++ general_inplace_merge_sort.h
@@ -7,45 +7,44 @@
 
 extern void gip_merge_sort(void* p, size_t n, size_t size,int (*compar) (const void*, const void*)) __nonnull ((1,4));
 
-void internal_sorting(void* p, size_t l, size_t r, size_t size, int (*compar) (const void*, const void*)) {
-	if(r-l == 1 || r-l==0)
-		return;
-
-	size_t s,m,f,i;
-	void* temp;
-
-	s=l;m=(l+r)/2;f=r;
-
-	internal_sorting(p, s, m, size, compar);
-	internal_sorting(p, m, r, size, compar);
-
-	temp = malloc((r-l)*size);
-
-	for(i=l;i<r;i++) {
-		if(s == (l+r)/2) {
-			MYCOPY(temp+(i-l)*size, p+m*size, size);
-			m++;
-		}
-		else if(m == r) {
-			MYCOPY(temp+(i-l)*size, p+s*size, size);
-			s++;
-		}
-		else if((*compar) (p+s*size, p+m*size) <= 0) {
-			MYCOPY(temp+(i-l)*size, p+s*size, size);
-			s++;
-		}
-		else {
-			MYCOPY(temp+(i-l)*size, p+m*size, size);
-			m++;
-		}
-	}
-	for(i=l;i<r;i++)
-		MYCOPY(p+i*size, temp+(i-l)*size, size);
-	free(temp);
-	return;
-}
-
 void gip_merge_sort(void* p, size_t n, size_t size,int (*compar) (const void*, const void*)) {
-	internal_sorting(p, 0, n, size, compar);
+	void internal_sorting(size_t l, size_t r) {
+		if(r-l == 1 || r-l==0)
+			return;
+
+		size_t s,m,f,i;
+		void* temp;
+
+		s=l;m=(l+r)/2;f=r;
+
+		internal_sorting(s, m);
+		internal_sorting(m, r);
+
+		temp = malloc((r-l)*size);
+
+		for(i=l;i<r;i++) {
+			if(s == (l+r)/2) {
+				MYCOPY(temp+(i-l)*size, p+m*size, size);
+				m++;
+			}
+			else if(m == r) {
+				MYCOPY(temp+(i-l)*size, p+s*size, size);
+				s++;
+			}
+			else if((*compar) (p+s*size, p+m*size) <= 0) {
+				MYCOPY(temp+(i-l)*size, p+s*size, size);
+				s++;
+			}
+			else {
+				MYCOPY(temp+(i-l)*size, p+m*size, size);
+				m++;
+			}
+		}
+		for(i=l;i<r;i++)
+			MYCOPY(p+i*size, temp+(i-l)*size, size);
+		free(temp);
+		return;
+	}
+	internal_sorting(0, n);
 }
 #endif
