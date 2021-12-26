f2af1537649ff1900e6bfc7aabd5b0b0ab17caca
venky <venky@g7>
2021-12-26 19:16:34 +0530

    Removed redundant allocations and freeing. Used a single allocation.

--- general_inplace_merge_sort.h
+++ general_inplace_merge_sort.h
@@ -7,44 +7,44 @@
 
 extern void gip_merge_sort(void* p, size_t n, size_t size,int (*compar) (const void*, const void*)) __nonnull ((1,4));
 
-void gip_merge_sort(void* p, size_t n, size_t size,int (*compar) (const void*, const void*)) {
+void gip_merge_sort(void* p, size_t n, size_t size,int (*compar) (const void*, const void*)) {	
+	void* temp;
+	temp = malloc(n*size);
+
 	void internal_sorting(size_t l, size_t r) {
 		if(r-l == 1 || r-l==0)
 			return;
 
 		size_t s,m,f,i;
-		void* temp;
 
 		s=l;m=(l+r)/2;f=r;
 
 		internal_sorting(s, m);
 		internal_sorting(m, r);
 
-		temp = malloc((r-l)*size);
-
 		for(i=l;i<r;i++) {
 			if(s == (l+r)/2) {
-				MYCOPY(temp+(i-l)*size, p+m*size, size);
+				MYCOPY(temp+i*size, p+m*size, size);
 				m++;
 			}
 			else if(m == r) {
-				MYCOPY(temp+(i-l)*size, p+s*size, size);
+				MYCOPY(temp+i*size, p+s*size, size);
 				s++;
 			}
 			else if((*compar) (p+s*size, p+m*size) <= 0) {
-				MYCOPY(temp+(i-l)*size, p+s*size, size);
+				MYCOPY(temp+i*size, p+s*size, size);
 				s++;
 			}
 			else {
-				MYCOPY(temp+(i-l)*size, p+m*size, size);
+				MYCOPY(temp+i*size, p+m*size, size);
 				m++;
 			}
 		}
 		for(i=l;i<r;i++)
-			MYCOPY(p+i*size, temp+(i-l)*size, size);
-		free(temp);
+			MYCOPY(p+i*size, temp+i*size, size);
 		return;
 	}
 	internal_sorting(0, n);
+	free(temp);
 }
 #endif
