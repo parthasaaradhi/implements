8693905779deb4918463e153e236f74f4fb7603b
venky <venkateshnagavolu@gmail.com>
2022-01-23 07:29:59 +0530

    Use memcopy instead of manual copy

--- general_inplace_merge_sort.h
+++ general_inplace_merge_sort.h
@@ -2,8 +2,7 @@
 #define GENERAL_INPLACE_MERGE_SORT
 
 #include<stdlib.h>
-
-#define MYCOPY(a,b,size) do { size_t __size = (size); char * __a = (a), *__b = (b); do { *__a = *__b; __a++; __b++; } while(--__size > 0); }while(0)
+#include<string.h>
 
 void gip_merge_sort(void* p, size_t n, size_t size,int (*compar) (const void*, const void*)) {	
 	void* temp;
@@ -22,24 +21,24 @@ void gip_merge_sort(void* p, size_t n, size_t size,int (*compar) (const void*, c
 
 		for(i=l;i<r;i++) {
 			if(s == (l+r)/2) {
-				MYCOPY(temp+i*size, p+m*size, size);
+				memcpy(temp+i*size, p+m*size, size);
 				m++;
 			}
 			else if(m == r) {
-				MYCOPY(temp+i*size, p+s*size, size);
+				memcpy(temp+i*size, p+s*size, size);
 				s++;
 			}
 			else if((*compar) (p+s*size, p+m*size) <= 0) {
-				MYCOPY(temp+i*size, p+s*size, size);
+				memcpy(temp+i*size, p+s*size, size);
 				s++;
 			}
 			else {
-				MYCOPY(temp+i*size, p+m*size, size);
+				memcpy(temp+i*size, p+m*size, size);
 				m++;
 			}
 		}
-		for(i=l;i<r;i++)
-			MYCOPY(p+i*size, temp+i*size, size);
+
+		memcpy(p+l*size, temp+l*size, (r-l)*size);
 		return;
 	}
 	internal_sorting(0, n);
